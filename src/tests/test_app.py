4d3379271fc7b90328e9f590a676e02a68a9344a
Vitor Morais <vitorm242017@gmail.com>
2026-05-30 17:29:19 -0300

    conflito com o banco de dados

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -3,11 +3,14 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from app import app
+# IMPORTANTE: Adicionamos o init_db na linha abaixo
+from app import app, init_db 
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
+    # IMPORTANTE: Mandamos o robô criar o banco de dados antes de iniciar o teste
+    init_db() 
     with app.test_client() as client:
         yield client
 
