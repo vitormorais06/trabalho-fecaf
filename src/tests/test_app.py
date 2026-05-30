e5d4b97e356a7f41872f44f4f72c395066ad6e64
Vitor Morais <65604596+vitormorais06@users.noreply.github.com>
2026-05-30 17:24:38 -0300

    Create test_app.py

---
+++ src/tests/test_app.py
@@ -0,0 +1,16 @@
+import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_homepage(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
